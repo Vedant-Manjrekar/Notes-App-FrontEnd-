1f38fa4c7fbe0d616908a76f5ec8dcffff8bd169
Vedant Manjrekar <vedant.28m@gmail.com>
2022-10-13 15:44:42 +0530

    Debugging for hosting.

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://notes-app-six-rho.vercel.app",
+  baseURL: "https://notes-web-application1.herokuapp.com",
 });
 
 export default instance;
